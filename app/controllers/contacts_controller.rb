a99882271890e2c37e038d4cd85a1cae1e04085a
Dheeraj Avvari <dj.mech.iitkgp@gmail.com>
2018-04-06 05:41:42 +0530

    Pagination with 10 per page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,13 +5,23 @@ class ContactsController < ApplicationController
   # GET /users/:user_id/contacts
   def index
     contact_search_data = contact_search_params
+    per_page = 10
+    if contact_search_data[:page].present? && contact_search_data[:page].to_i > 0
+      page = contact_search_data[:page].to_i
+    else
+      page = 1
+    end
+    offset = (page - 1) * per_page
     if contact_search_data[:q].present?
       query = '%' + contact_search_data[:q] + '%'
-      contacts = @user.contacts.where('contacts.email LIKE ? OR contacts.firstname LIKE ? OR contacts.lastname LIKE ?', query, query, query)
+      total_count = @user.contacts.where('contacts.email LIKE ? OR contacts.firstname LIKE ? OR contacts.lastname LIKE ?', query, query, query).count
+      contacts = @user.contacts.where('contacts.email LIKE ? OR contacts.firstname LIKE ? OR contacts.lastname LIKE ?', query, query, query).limit(per_page).offset(offset)
     else
-      contacts = @user.contacts
+      total_count = @user.contacts.count
+      contacts = @user.contacts.limit(per_page).offset(offset)
     end
-    json_response({ contacts: contacts })
+    total_pages = (total_count.to_f / per_page.to_f).ceil
+    json_response({ contacts: contacts, total_pages: total_pages, page: page })
   end
 
   # GET /users/:user_id/contacts/:id
@@ -54,7 +64,7 @@ class ContactsController < ApplicationController
   end
 
   def contact_search_params
-    params.permit(:q, :user_id)
+    params.permit(:q, :page, :user_id)
   end
 
   def set_user
